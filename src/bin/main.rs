4aecdd6a0ef6f5e6853e99878979e552f33341c4
cdrani <charlesdrani@gmail.com>
2024-03-26 13:04:15 -0600

    refactor: use clap to run bin in cli

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,35 +2,57 @@
 
 extern crate triword;
 
-use triword::solver::{get_word_list, solve};
+use clap::{arg, Command};
+use triword::{
+    generator::generate,
+    solver::{get_word_list, solve},
+};
 
 fn main() {
-    let word_search = get_word_list();
-
-    let args: Vec<String> = std::env::args().collect();
-
-    if args.len() < 3 {
-        println!("Usage: <puzzle> <size>");
-        return;
-    }
-
-    let puzzle = &args[1];
-    let size: usize = if let Ok(s) = args[2].parse() {
-        s
-    } else {
-        println!("Invalid Size. Size must be >= 3");
-        return;
-    };
-
-    let result = solve(puzzle, size, &word_search);
-
-    if result.solution.is_empty() {
-        println!("No solution found for puzzle {puzzle} with size {size}. Please verify the puzzle and size.");
-    } else {
-        println!(
-            "Solution for puzzle {puzzle} with size {size} is {}.",
-            result.solution
+    let cmd = Command::new("triword")
+        .bin_name("triword")
+        .about("Triword Puzzle Generator and Solver")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("solve")
+                .about("Solve puzzle")
+                .arg(arg!(<PUZZLE> "Puzzle to solve"))
+                .arg(arg!(<SIZE> "Puzzle key size").value_parser(clap::value_parser!(usize)))
+                .arg_required_else_help(true),
+        )
+        .subcommand(
+            Command::new("generate")
+                .about("Generate random puzzle unless key is provided")
+                .arg(arg!(<KEY> "Generate puzzle from key").required(false)),
         );
-        println!("Words in puzzle: {:?}.", &result.words_used[..=2]);
+
+    let matches = cmd.get_matches();
+
+    match matches.subcommand() {
+        Some(("solve", submatches)) => {
+            let puzzle = submatches.get_one::<String>("PUZZLE").expect("required");
+            let size = *submatches.get_one::<usize>("SIZE").expect("required");
+
+            let word_search = get_word_list();
+            let result = solve(puzzle, size, &word_search);
+
+            if result.solution.is_empty() {
+                println!("No solution found for puzzle {puzzle} with size {size}. Please verify the puzzle and size.");
+            } else {
+                println!(
+                    "Solution for puzzle {puzzle} with size {size} is {}.",
+                    result.solution
+                );
+                println!("Words in puzzle: {:?}.", &result.words_used[..=2]);
+            }
+        }
+        Some(("generate", submatches)) => {
+            let key = submatches
+                .get_one::<String>("KEY")
+                .map(std::clone::Clone::clone);
+            let _ = generate(key);
+        }
+        _ => unreachable!(),
     }
 }
